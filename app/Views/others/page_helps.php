b8229d49bf5e71330f193dff6e74dcd0453c36c0
samriadi <basosamriadi01@gmail.com>
2024-06-13 14:45:02 +0800

    update page helps

--- app/Views/others/page_helps.php
+++ app/Views/others/page_helps.php
@@ -1,45 +1,10 @@
-<!-- 
- <div class="modal fade" id="helpModal" tabindex="-1" aria-labelledby="helModalLabel" aria-hidden="true">
-   <div class="modal-dialog">
-     <div class="modal-content">
-       <div class="modal-header">
-         <h5 class="modal-title" id="helModalLabel">Helps</h5>
-         <button type="button" class="btn btn-close" data-bs-dismiss="modal" aria-label="Close">x
-         </button>
-       </div>
-       <div class="modal-body">
-         <input type="hidden" class="form-control" id="recid" name="recid" required>
-         <div class="form-group">
-           <label for="editPage">Pages</label>
-           <input type="text" class="form-control" id="editPage" name="page" required>
-         </div>
-         <div class="form-group">
-           <label for="editKonten">Konten</label>
-           <textarea class="form-control" id="editKonten" name="konten" required></textarea>
-         </div>
-       </div>
-       <div class="modal-footer">
-         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-         <button type="button" class="btn btn-primary" id="update">Save</button>
-       </div>
-     </div>
-   </div>
- </div> -->
-
-<!-- page_helps.php -->
-
 <div class="modal-body">
-  <input type="hidden" class="form-control" id="recid" name="recid" required>
   <div class="form-group">
-    <label for="editPage">Pages</label>
-    <input type="text" class="form-control" id="editPage" name="page" required>
+    <label for="editPage">Page</label>
+    <input type="text" class="form-control" id="editPage" name="page">
   </div>
   <div class="form-group">
     <label for="editKonten">Konten</label>
     <textarea class="form-control" id="editKonten" name="konten" required></textarea>
   </div>
-</div>
-<div class="modal-footer">
-  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-  <button type="button" class="btn btn-primary" id="update">Save</button>
 </div>
