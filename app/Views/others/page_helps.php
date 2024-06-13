cd6521ed9b587df8b2ce05f7befc91bfc2b26508
samriadi <basosamriadi01@gmail.com>
2024-06-13 15:33:17 +0800

    update page modal help

--- app/Views/others/page_helps.php
+++ app/Views/others/page_helps.php
@@ -1,8 +1,5 @@
 <div class="modal-body">
-  <div class="form-group">
-    <label for="editPage">Page</label>
-    <input type="text" class="form-control" id="editPage" name="page">
-  </div>
+  <input type="hidden" class="form-control" id="editPage" name="page">
   <div class="form-group">
     <label for="editKonten">Konten</label>
     <textarea class="form-control" id="editKonten" name="konten" required></textarea>
