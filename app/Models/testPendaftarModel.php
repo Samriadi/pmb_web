e3d30811858e93aaeb80719912cd4f2957a9629e
samriadi <basosamriadi01@gmail.com>
2024-07-10 17:06:51 +0800

    make testpendaftar model

---
+++ app/Models/testPendaftarModel.php
@@ -0,0 +1,81 @@
+<?php
+class testPendaftarModel
+{
+    // public function getPendaftarTerverifikasi()
+    // {
+    //     $db = Database::getInstance();
+    //     $query = "SELECT a.*, b.NamaLengkap 
+    //                 FROM pmb_tagihan a 
+    //                 LEFT JOIN pmb_mahasiswa b ON b.ID = a.member_id 
+    //                 WHERE a.verified = 'Verified' 
+    //                 AND NOT EXISTS (
+    //                     SELECT 1
+    //                     FROM pmb_jadualtes j
+    //                     WHERE j.test_memberid = a.member_id
+    //             )";
+    //     $stmt = $db->prepare($query);
+    //     $stmt->execute();
+    //     return $stmt->fetchAll(PDO::FETCH_OBJ);
+    // }
+
+    public function getPendaftarTerverifikasi()
+    {
+        $db = Database::getInstance();
+        $query = "SELECT 
+                        a.id,
+                        a.verified,
+                        a.member_id,
+                        a.no_ujian,
+                        a.jenis,
+                        a.jenjang,
+                        b.ID,
+                        b.NamaLengkap,
+                        c.Periode,
+                        COALESCE(d1.var_value, '') AS Prodi1,
+                        COALESCE(d2.var_value, '') AS Prodi2,
+                        COALESCE(d3.var_value, '') AS Prodi3
+                    FROM 
+                        pmb_tagihan a
+                    LEFT JOIN 
+                        pmb_mahasiswa b ON b.ID = a.member_id
+                    LEFT JOIN 
+                        pmb_periode c ON c.recid = a.periode
+                    LEFT JOIN 
+                        varoption d1 ON d1.recid = a.PilihanPertama
+                    LEFT JOIN 
+                        varoption d2 ON d2.recid = a.PilihanKedua
+                    LEFT JOIN 
+                        varoption d3 ON d3.recid = a.PilihanKetiga
+                    WHERE a.verified = 'Verified'   
+                    AND NOT EXISTS (
+                        SELECT 1
+                        FROM pmb_jadualtes e
+                        WHERE e.test_memberid = a.member_id)";
+        $stmt = $db->prepare($query);
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_OBJ);
+    }
+
+    public function addTestPendaftar($member_id, $test_tanggal, $test_mulai, $test_selesai, $test_lokasi)
+    {
+        $db = Database::getInstance();
+
+        $query = "INSERT INTO pmb_jadualtes (test_tanggal, test_mulai, test_selesai, test_lokasi, test_memberid) VALUES (?, ?, ?, ?, ?)";
+
+        try {
+            $stmt = $db->prepare($query);
+            $stmt->execute([$test_tanggal, $test_mulai, $test_selesai, $test_lokasi, $member_id]);
+        } catch (PDOException $e) {
+            echo "Error: " . $e->getMessage();
+        }
+    }
+
+    public function getTestPendaftar(){
+        $db = Database::getInstance();
+        $query = "SELECT * FROM pmb_jadualtes";
+        $stmt = $db->prepare($query);
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_OBJ);
+    }
+
+}
