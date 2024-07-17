fd68cbbf84c403b04eed223841a487b1c59d2b18
samriadi <basosamriadi01@gmail.com>
2024-07-18 05:27:49 +0800

    add global variabel name table

---
+++ app/Core/Table.php
@@ -0,0 +1,16 @@
+<?php
+$pmb_mahasiswa = "pmb_mahasiswa";
+$pmb_tagihan = "pmb_tagihan";
+$pmb_jadualtes = "pmb_jadualtes";
+$pmb_periode = "pmb_periode";
+$pmb_nim = "pmb_nim";
+$pmb_jadualtes = "pmb_jadualtes";
+$pmb_test = "pmb_test";
+$pmb_helps = "pmb_helps";
+$pmb_logs = "pmb_logs";
+$pmb_pembayaran = "pmb_pembayaran";
+$pmb_promo = "pmb_promo";
+
+$varoption = "varoption";
+$varinstall = "varinstall";
+$usrapp = "usrapp";
