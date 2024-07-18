e6e5390f164404021e0bb77b49e47efd8d2c01b8
samriadi <basosamriadi01@gmail.com>
2024-07-18 15:08:08 +0800

    update prodi model

--- app/Models/prodiModel.php
+++ app/Models/prodiModel.php
@@ -3,36 +3,43 @@
 
 class prodiModel
 {
+
+    private $varoption;
+    private $db;
+    public function __construct()
+    {
+        global $varoption;
+        $this->varoption = $varoption;
+        $this->db = Database::getInstance();
+    }
+
     public function getProdi()
     {
-        $db = Database::getInstance();
         $query = "SELECT 
-                        varoption.*, 
+                        $this->varoption.*, 
                         Fakultas.var_value AS NamaFakultas,
                         Jenjang.var_others AS Biaya,
                         Jenjang.catatan AS Catatan
                     FROM 
-                        varoption 
+                        $this->varoption 
                     LEFT JOIN 
-                        (SELECT * FROM varoption WHERE var_name='Fakultas') AS Fakultas 
-                        ON varoption.parent = Fakultas.recid
+                        (SELECT * FROM $this->varoption WHERE var_name='Fakultas') AS Fakultas 
+                        ON $this->varoption.parent = Fakultas.recid
                     LEFT JOIN 
-                        (SELECT * FROM varoption WHERE var_name='Jenjang') AS Jenjang 
-                        ON varoption.var_others = Jenjang.var_value
+                        (SELECT * FROM $this->varoption WHERE var_name='Jenjang') AS Jenjang 
+                        ON $this->varoption.var_others = Jenjang.var_value
                     WHERE 
-                        varoption.var_name='Prodi';";
-        $stmt = $db->prepare($query);
+                        $this->varoption.var_name='Prodi';";
+        $stmt = $this->db->prepare($query);
         $stmt->execute();
         return $stmt->fetchAll(PDO::FETCH_OBJ);
     }
     public function add($varname, $varvalue, $varothers, $parent)
     {
-        $db = Database::getInstance();
-
-        $query = "INSERT INTO varoption (var_name, var_value, var_others, parent) VALUES (?, ?, ?, ?)";
+        $query = "INSERT INTO $this->varoption (var_name, var_value, var_others, parent) VALUES (?, ?, ?, ?)";
 
         try {
-            $stmt = $db->prepare($query);
+            $stmt = $this->db->prepare($query);
             $stmt->execute([$varname, $varvalue, $varothers, $parent]);
         } catch (PDOException $e) {
             echo "Error: " . $e->getMessage();
@@ -40,26 +47,22 @@ class prodiModel
     }
     public function getVarById($recid)
     {
-        $db = Database::getInstance();
-        $stmt = $db->prepare("SELECT * FROM varoption WHERE recid = ?");
+        $stmt = $this->db->prepare("SELECT * FROM $this->varoption WHERE recid = ?");
         $stmt->execute([$recid]);
         return $stmt->fetch(PDO::FETCH_ASSOC);
     }
 
     public function getVarByName($var_name)
     {
-        $db = Database::getInstance();
-        $query = "SELECT * FROM varoption where var_name=:var_name";
-        $stmt = $db->prepare($query);
+        $query = "SELECT * FROM $this->varoption where var_name=:var_name";
+        $stmt = $this->db->prepare($query);
         $stmt->bindParam(':var_name', $var_name);
         $stmt->execute();
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
     public function updateVar($recid, $varvalue, $varothers, $parent)
     {
-        $db = Database::getInstance();
-
-        $stmt = $db->prepare("UPDATE varoption SET var_value = ?, var_others = ?, parent = ? WHERE recid = ?");
+        $stmt = $this->db->prepare("UPDATE $this->varoption SET var_value = ?, var_others = ?, parent = ? WHERE recid = ?");
         $stmt->execute([$varvalue, $varothers, $parent, $recid]);
     }
 }
