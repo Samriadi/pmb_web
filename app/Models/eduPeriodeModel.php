4699d47059b4ec6bac405eaa9f296a28c86c0918
samriadi <basosamriadi01@gmail.com>
2024-07-18 15:05:53 +0800

    update eduperiode model

--- app/Models/eduPeriodeModel.php
+++ app/Models/eduPeriodeModel.php
@@ -3,12 +3,23 @@
 
 class eduPeriodeModel
 {
+    private $pmb_periode;
+    private $pmb_tagihan;
+    private $db;
+
+    public function __construct(){
+        global $pmb_periode;
+        global $pmb_tagihan;
+
+        $this->pmb_periode = $pmb_periode;
+        $this->pmb_tagihan = $pmb_tagihan;
+        $this->db = Database::getInstance();
+    }
     public function getPeriode()
     {
         try {
-            $db = Database::getInstance();
-            $query = "SELECT DISTINCT a.*, IF(b.periode IS NOT NULL, 'true', 'false') AS isInTagihan FROM pmb_periode a LEFT JOIN pmb_tagihan b ON b.periode = a.recid";
-            $stmt = $db->prepare($query);
+            $query = "SELECT DISTINCT a.*, IF(b.periode IS NOT NULL, 'true', 'false') AS isInTagihan FROM $this->pmb_periode a LEFT JOIN $this->pmb_tagihan b ON b.periode = a.recid";
+            $stmt = $this->db->prepare($query);
             $stmt->execute();
             return $stmt->fetchAll(PDO::FETCH_OBJ);
         } catch (PDOException $e) {
@@ -20,13 +31,12 @@ class eduPeriodeModel
     public function addPeriode($jenjang, $periode, $fromDate, $toDate, $keterangan, $status)
     {
 
-        $db = Database::getInstance();
 
-        $query = "INSERT INTO pmb_periode (Jenjang, Periode, fromDate, toDate, Keterangan, status) VALUES (?, ?, ?, ?, ?, ?)";
+        $query = "INSERT INTO $this->pmb_periode (Jenjang, Periode, fromDate, toDate, Keterangan, status) VALUES (?, ?, ?, ?, ?, ?)";
 
         try {
 
-            $stmt = $db->prepare($query);
+            $stmt = $this->db->prepare($query);
             $success = $stmt->execute([$jenjang, $periode, $fromDate, $toDate, $keterangan, $status]);
 
             if ($success) {
@@ -47,18 +57,16 @@ class eduPeriodeModel
 
     public function getPeriodeById($id)
     {
-        $db = Database::getInstance();
-        $stmt = $db->prepare("SELECT * FROM pmb_periode WHERE recid = ?");
+        $stmt = $this->db->prepare("SELECT * FROM $this->pmb_periode WHERE recid = ?");
         $stmt->execute([$id]);
         return $stmt->fetch(PDO::FETCH_ASSOC);
     }
 
     public function updatePeriode($recid, $jenjang, $periode, $fromDate, $toDate, $keterangan, $status)
     {
-        $db = Database::getInstance();
 
         try {
-            $stmt = $db->prepare("UPDATE pmb_periode SET Jenjang = ?, Periode = ?, fromDate = ?, toDate = ?, Keterangan = ?, status = ?  WHERE recid = ?");
+            $stmt = $this->db->prepare("UPDATE $this->pmb_periode SET Jenjang = ?, Periode = ?, fromDate = ?, toDate = ?, Keterangan = ?, status = ?  WHERE recid = ?");
             $stmt->execute([$jenjang, $periode, $fromDate, $toDate, $keterangan, $status, $recid]);
             return ['status' => 'success', 'message' => 'Record updated successfully'];
         } catch (PDOException $e) {
@@ -73,12 +81,11 @@ class eduPeriodeModel
 
     public function deletePeriode($id)
     {
-        $db = Database::getInstance();
 
-        $query = "DELETE FROM pmb_periode WHERE recid = ?";
+        $query = "DELETE FROM $this->pmb_periode WHERE recid = ?";
 
         try {
-            $stmt = $db->prepare($query);
+            $stmt = $this->db->prepare($query);
             $stmt->execute([$id]);
         } catch (PDOException $e) {
             echo "Error: " . $e->getMessage();
@@ -87,8 +94,7 @@ class eduPeriodeModel
 
     public function getLastPeriode($jenjang)
     {
-        $db = Database::getInstance();
-        $stmt = $db->prepare("SELECT ifnull(max(periode), 0) as lastPeriod FROM pmb_periode where Jenjang=? and status = 'Open'");
+        $stmt = $this->db->prepare("SELECT ifnull(max(periode), 0) as lastPeriod FROM $this->pmb_periode where Jenjang=? and status = 'Open'");
         $stmt->execute([$jenjang]);
         return $stmt->fetch(PDO::FETCH_ASSOC);
     }
