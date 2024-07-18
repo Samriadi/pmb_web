9f249e7d25b05b6a5ba663d7b173be88299e7d50
samriadi <basosamriadi01@gmail.com>
2024-07-18 15:07:44 +0800

    update pembayaran model

--- app/Models/pembayaranModel.php
+++ app/Models/pembayaranModel.php
@@ -1,9 +1,25 @@
 <?php
 class pembayaranModel
 {
+    
+    private $pmb_pembayaran;
+    private $pmb_nim;
+    private $pmb_mahasiswa;
+    private $db;
+    public function __construct()
+    {
+        global $pmb_pembayaran;
+        global $pmb_nim;
+        global $pmb_mahasiswa;
+
+        $this->pmb_pembayaran = $pmb_pembayaran;
+        $this->pmb_nim = $pmb_nim;
+        $this->pmb_mahasiswa = $pmb_mahasiswa;
+        $this->db = Database::getInstance();
+    }
+
     public function getPembayaran()
     {
-        $db = Database::getInstance();
         $query = "SELECT 
                     p.recid, 
                     p.member_id, 
@@ -19,49 +35,47 @@ class pembayaranModel
                     END AS isHaveNim,
                     m.NamaLengkap
                 FROM 
-                    pmb_pembayaran p
+                    $this->pmb_pembayaran p
                 LEFT JOIN 
-                    pmb_nim n ON p.member_id = n.member_id
+                    $this->pmb_nim n ON p.member_id = n.member_id
                 LEFT JOIN
-                	pmb_mahasiswa m ON m.ID = p.member_id
+                	$this->pmb_mahasiswa m ON m.ID = p.member_id
                 WHERE 
                     n.member_id IS NULL;
                 ";
-        $stmt = $db->prepare($query);
+        $stmt = $this->db->prepare($query);
         $stmt->execute();
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 
     public function getNIM()
     {
-        $db = Database::getInstance();
         $query = "SELECT 
                     a.*,
-                    m.NamaLengkap 
-                    FROM pmb_nim a
+                    m.NamaLengkap,
+                    p.prodi 
+                    FROM $this->pmb_nim a
                     LEFT JOIN 
-                    pmb_mahasiswa m ON m.ID = a.member_id
+                    $this->pmb_mahasiswa m ON m.ID = a.member_id
+                    LEFT JOIN $this->pmb_pembayaran p ON p.member_id = a.member_id
                 ";
-        $stmt = $db->prepare($query);
+        $stmt = $this->db->prepare($query);
         $stmt->execute();
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 
     public function saveNIM($data)
     {
-        $db = Database::getInstance();
         foreach ($data as $key => $value) {
-            $query = "INSERT INTO pmb_nim (member_id, angkatan, jenjang, kategori, jenis, prodi_id, nim) VALUES (?, ?, ?, ?, ?, ?, ?)";
-            $stmt = $db->prepare($query);
+            $query = "INSERT INTO $this->pmb_nim (member_id, angkatan, jenjang, kategori, jenis, prodi_id, nim) VALUES (?, ?, ?, ?, ?, ?, ?)";
+            $stmt = $this->db->prepare($query);
             $stmt->execute([$value['member_id'], $value['periode'], $value['jenjang'], $value['kategori'], $value['jenis'], $value['prodi_id'], $value['nim']]);
         }
     }
     public function getCountNIM($prodi_id, $kategori)
     {
-        $db = Database::getInstance();
-
-        $query = "SELECT COUNT(*) FROM pmb_nim WHERE prodi_id = :prodi_id AND kategori = :kategori";
-        $stmt = $db->prepare($query);
+        $query = "SELECT COUNT(*) FROM $this->pmb_nim WHERE prodi_id = :prodi_id AND kategori = :kategori";
+        $stmt = $this->db->prepare($query);
         $stmt->bindParam(':prodi_id', $prodi_id, PDO::PARAM_INT);
         $stmt->bindParam(':kategori', $kategori, PDO::PARAM_STR);
         $stmt->execute();
