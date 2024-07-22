ea9c6c3de4b88a7d2bf249568a0a9fc1b27939a0
samriadi <basosamriadi01@gmail.com>
2024-07-23 04:55:14 +0800

    update pembayaran controller

--- app/Controllers/pembayaranController.php
+++ app/Controllers/pembayaranController.php
@@ -1,6 +1,12 @@
 <?php
 class PembayaranController
 {
+    private $response = [];
+
+    public function __construct()
+    {
+        $this->response = []; // Reset response
+    }
     public function index()
     {
         $models = new pembayaranModel();
@@ -74,193 +80,69 @@ class PembayaranController
         $inputJSON = file_get_contents('php://input');
         $input = json_decode($inputJSON, true);
 
-        $models = new pembayaranModel();
         $data = $input['filteredData'];
 
-        $dataApotekerReguler = $dataApotekerTransfer = [];
-        $dataS1FarmasiReguler = $dataS1FarmasiTransfer = [];
-        $dataD3FarmasiReguler = $dataD3FarmasiTransfer = [];
-        $dataKebidananReguler = $dataKebidananTransfer = [];
-        $dataAkuntansiReguler = $dataAkuntansiTransfer = [];
-        $dataHukumReguler = $dataHukumTransfer = [];
-        $dataIlmuKomunikasiReguler = $dataIlmuKomunikasiTransfer = [];
-        $dataManajemenReguler = $dataManajemenTransfer = [];
-        $dataInformatikaReguler = $dataInformatikaTransfer = [];
-        $dataSistemInformasiReguler = $dataSistemInformasiTransfer = [];
+        $categories = [
+            "01" => ['Reguler' => [], 'Transfer' => []],
+            "02" => ['Reguler' => [], 'Transfer' => []],
+            "03" => ['Reguler' => [], 'Transfer' => []],
+            "04" => ['Reguler' => [], 'Transfer' => []],
+            "05" => ['Reguler' => [], 'Transfer' => []],
+            "06" => ['Reguler' => [], 'Transfer' => []],
+            "07" => ['Reguler' => [], 'Transfer' => []],
+            "08" => ['Reguler' => [], 'Transfer' => []],
+            "09" => ['Reguler' => [], 'Transfer' => []],
+            "10" => ['Reguler' => [], 'Transfer' => []],
+        ];
 
         foreach ($data as $dt) {
-            switch ($dt['prodi_id']) {
-                case "01":
-                    if ($dt['kategori'] == "Reguler") {
-                        $dataApotekerReguler = $this->filterData($data, $dt['prodi_id'], $dt['kategori']);
-                    } elseif ($dt['kategori'] == "Transfer") {
-                        $dataApotekerTransfer = $this->filterData($data, $dt['prodi_id'], $dt['kategori']);
-                    }
-                    break;
-                case "02":
-                    if ($dt['kategori'] == "Reguler") {
-                        $dataS1FarmasiReguler = $this->filterData($data, $dt['prodi_id'], $dt['kategori']);
-                    } elseif ($dt['kategori'] == "Transfer") {
-                        $dataS1FarmasiTransfer = $this->filterData($data, $dt['prodi_id'], $dt['kategori']);
-                    }
-                    break;
-                case "03":
-                    if ($dt['kategori'] == "Reguler") {
-                        $dataD3FarmasiReguler = $this->filterData($data, $dt['prodi_id'], $dt['kategori']);
-                    } elseif ($dt['kategori'] == "Transfer") {
-                        $dataD3FarmasiTransfer = $this->filterData($data, $dt['prodi_id'], $dt['kategori']);
-                    }
-                    break;
-                case "04":
-                    if ($dt['kategori'] == "Reguler") {
-                        $dataKebidananReguler = $this->filterData($data, $dt['prodi_id'], $dt['kategori']);
-                    } elseif ($dt['kategori'] == "Transfer") {
-                        $dataKebidananTransfer = $this->filterData($data, $dt['prodi_id'], $dt['kategori']);
-                    }
-                    break;
-                case "05":
-                    if ($dt['kategori'] == "Reguler") {
-                        $dataAkuntansiReguler = $this->filterData($data, $dt['prodi_id'], $dt['kategori']);
-                    } elseif ($dt['kategori'] == "Transfer") {
-                        $dataAkuntansiTransfer = $this->filterData($data, $dt['prodi_id'], $dt['kategori']);
-                    }
-                    break;
-                case "06":
-                    if ($dt['kategori'] == "Reguler") {
-                        $dataHukumReguler = $this->filterData($data, $dt['prodi_id'], $dt['kategori']);
-                    } elseif ($dt['kategori'] == "Transfer") {
-                        $dataHukumTransfer = $this->filterData($data, $dt['prodi_id'], $dt['kategori']);
-                    }
-                    break;
-                case "07":
-                    if ($dt['kategori'] == "Reguler") {
-                        $dataIlmuKomunikasiReguler = $this->filterData($data, $dt['prodi_id'], $dt['kategori']);
-                    } elseif ($dt['kategori'] == "Transfer") {
-                        $dataIlmuKomunikasiTransfer = $this->filterData($data, $dt['prodi_id'], $dt['kategori']);
-                    }
-                    break;
-                case "08":
-                    if ($dt['kategori'] == "Reguler") {
-                        $dataManajemenReguler = $this->filterData($data, $dt['prodi_id'], $dt['kategori']);
-                    } elseif ($dt['kategori'] == "Transfer") {
-                        $dataManajemenTransfer = $this->filterData($data, $dt['prodi_id'], $dt['kategori']);
-                    }
-                    break;
-                case "09":
-                    if ($dt['kategori'] == "Reguler") {
-                        $dataInformatikaReguler = $this->filterData($data, $dt['prodi_id'], $dt['kategori']);
-                    } elseif ($dt['kategori'] == "Transfer") {
-                        $dataInformatikaTransfer = $this->filterData($data, $dt['prodi_id'], $dt['kategori']);
-                    }
-                    break;
-                case "10":
-                    if ($dt['kategori'] == "Reguler") {
-                        $dataSistemInformasiReguler = $this->filterData($data, $dt['prodi_id'], $dt['kategori']);
-                    } elseif ($dt['kategori'] == "Transfer") {
-                        $dataSistemInformasiTransfer = $this->filterData($data, $dt['prodi_id'], $dt['kategori']);
-                    }
-                    break;
+            $prodiId = $dt['prodi_id'];
+            $kategori = $dt['kategori'];
+
+            if (isset($categories[$prodiId][$kategori])) {
+                $categories[$prodiId][$kategori][] = $dt;
             }
         }
 
-        if (!empty($dataS1FarmasiReguler)) {
-            $NimS1FarmasiRegular = $this->makeNimAll($dataS1FarmasiReguler);
-            $models->saveNIM($NimS1FarmasiRegular);
+        foreach ($categories as $prodiId => $kategoris) {
+            foreach ($kategoris as $kategori => $items) {
+                if (!empty($items)) {
+                    $nim = $this->makeNimAll($items);
+                    $responseKey = $this->getResponseKey($prodiId, $kategori);
+                    $this->addToResponse($this->response, $responseKey, $nim);
+                }
+            }
         }
 
-        if (!empty($dataS1FarmasiTransfer)) {
-            $NimS1FarmasiTransfer = $this->makeNimAll($dataS1FarmasiTransfer);
-            $models->saveNIM($NimS1FarmasiTransfer);
-        }
+        header('Content-Type: application/json');
+        echo json_encode($this->response);
+    }
 
-        if (!empty($dataApotekerReguler)) {
-            $NimApotekerReguler = $this->makeNimAll($dataApotekerReguler);
-            $models->saveNIM($NimApotekerReguler);
-        }
+    private function getResponseKey($prodiId, $kategori)
+    {
+        $prodiNames = [
+            "01" => "Profesi-Apoteker",
+            "02" => "S1/RPL-Farmasi",
+            "03" => "D3-Farmasi",
+            "04" => "D3-Kebidanan",
+            "05" => "S1-Akuntansi",
+            "06" => "S1-Hukum",
+            "07" => "S1-IlmuKomunikasi",
+            "08" => "S1-Manajemen",
+            "09" => "S1-Informatika",
+            "10" => "S1-SistemInformasi"
+        ];
 
-        if (!empty($dataApotekerTransfer)) {
-            $NimApotekerTransfer = $this->makeNimAll($dataApotekerTransfer);
-            $models->saveNIM($NimApotekerTransfer);
-        }
+        return $prodiNames[$prodiId] . $kategori;
+    }
 
-        if (!empty($dataD3FarmasiReguler)) {
-            $NimD3FarmasiReguler = $this->makeNimAll($dataD3FarmasiReguler);
-            $models->saveNIM($NimD3FarmasiReguler);
-        }
 
-        if (!empty($dataD3FarmasiTransfer)) {
-            $NimD3FarmasiTransfer = $this->makeNimAll($dataD3FarmasiTransfer);
-            $models->saveNIM($NimD3FarmasiTransfer);
-        }
-
-        if (!empty($dataKebidananReguler)) {
-            $NimKebidananReguler = $this->makeNimAll($dataKebidananReguler);
-            $models->saveNIM($NimKebidananReguler);
-        }
-
-        if (!empty($dataKebidananTransfer)) {
-            $NimKebidananTransfer = $this->makeNimAll($dataKebidananTransfer);
-            $models->saveNIM($NimKebidananTransfer);
-        }
-
-        if (!empty($dataAkuntansiReguler)) {
-            $NimAkuntansiReguler = $this->makeNimAll($dataAkuntansiReguler);
-            $models->saveNIM($NimAkuntansiReguler);
-        }
-
-        if (!empty($dataAkuntansiTransfer)) {
-            $NimAkuntansiTransfer = $this->makeNimAll($dataAkuntansiTransfer);
-            $models->saveNIM($NimAkuntansiTransfer);
-        }
-
-        if (!empty($dataHukumReguler)) {
-            $NimHukumReguler = $this->makeNimAll($dataHukumReguler);
-            $models->saveNIM($NimHukumReguler);
-        }
-
-        if (!empty($dataHukumTransfer)) {
-            $NimHukumTransfer = $this->makeNimAll($dataHukumTransfer);
-            $models->saveNIM($NimHukumTransfer);
-        }
-
-        if (!empty($dataIlmuKomunikasiReguler)) {
-            $NimIlmuKomunikasiReguler = $this->makeNimAll($dataIlmuKomunikasiReguler);
-            $models->saveNIM($NimIlmuKomunikasiReguler);
-        }
-
-        if (!empty($dataIlmuKomunikasiTransfer)) {
-            $NimIlmuKomunikasiTransfer = $this->makeNimAll($dataIlmuKomunikasiTransfer);
-            $models->saveNIM($NimIlmuKomunikasiTransfer);
-        }
-
-        if (!empty($dataManajemenReguler)) {
-            $NimManajemenReguler = $this->makeNimAll($dataManajemenReguler);
-            $models->saveNIM($NimManajemenReguler);
-        }
-
-        if (!empty($dataManajemenTransfer)) {
-            $NimManajemenTransfer = $this->makeNimAll($dataManajemenTransfer);
-            $models->saveNIM($NimManajemenTransfer);
-        }
-
-        if (!empty($dataInformatikaReguler)) {
-            $NimInformatikaReguler = $this->makeNimAll($dataInformatikaReguler);
-            $models->saveNIM($NimInformatikaReguler);
-        }
-
-        if (!empty($dataInformatikaTransfer)) {
-            $NimInformatikaTransfer = $this->makeNimAll($dataInformatikaTransfer);
-            $models->saveNIM($NimInformatikaTransfer);
-        }
-
-        if (!empty($dataSistemInformasiReguler)) {
-            $NimSistemInformasiReguler = $this->makeNimAll($dataSistemInformasiReguler);
-            $models->saveNIM($NimSistemInformasiReguler);
-        }
-
-        if (!empty($dataSistemInformasiTransfer)) {
-            $NimSistemInformasiTransfer = $this->makeNimAll($dataSistemInformasiTransfer);
-            $models->saveNIM($NimSistemInformasiTransfer);
+    private function addToResponse(&$response, $key, $data)
+    {
+        $models = new pembayaranModel();
+        if (!empty($data)) {
+            $result = $models->saveNIM($data);
+            $response[$key] = $result;
         }
     }
 }
