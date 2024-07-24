e0150acf888a4745932be892a629c8f8a398df82
samriadi <basosamriadi01@gmail.com>
2024-07-24 15:45:35 +0800

    add kelulusanController

---
+++ app/Controllers/kelulusanController.php
@@ -0,0 +1,49 @@
+<?php
+
+
+class kelulusanController
+{
+
+  private $kelulusanModel;
+
+  public function __construct()
+  {
+    $this->kelulusanModel = new kelulusanModel();
+  }
+  public function kelulusan()
+  {
+    $data = $this->kelulusanModel->getKeteranganTes();
+    include __DIR__ . '/../Views/others/page_kelulusan.php';
+  }
+  public function getProdi()
+  {
+    if (isset($_POST['id'])) {
+      $id = $_POST['id'];
+      $data = $this->kelulusanModel->getProdiByNoUjian($id);
+
+      header('Content-Type: application/json');
+      echo json_encode($data);
+    } else {
+      echo json_encode(['error' => 'No examination number provided.']);
+    }
+  }
+
+  public function addKelulusan()
+  {
+    $inputJSON = file_get_contents('php://input');
+    $input = json_decode($inputJSON, true);
+
+    if (isset($input['data']) && is_array($input['data'])) {
+      $data = $input['data'];
+
+      foreach ($data as $dt) {
+        $idTagihan = $dt['idTagihan'];
+        $prodiLulus = $dt['prodiLulus'];
+      }
+    }
+
+    $response = $this->kelulusanModel->addKelulusan($idTagihan, $prodiLulus);
+    header('Content-Type: application/json');
+    echo json_encode($response);
+  }
+}
