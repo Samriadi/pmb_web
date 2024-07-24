b7961eeafc7c34c8abc68af334381646ec97e255
samriadi <basosamriadi01@gmail.com>
2024-07-24 17:01:47 +0800

    update auth login model

--- app/Models/loginModel.php
+++ app/Models/loginModel.php
@@ -5,23 +5,34 @@ class loginModel
   private $usrapp;
   private $db;
   public function __construct()
-    {
-        global $usrapp;
-        $this->usrapp = $usrapp;
-        $this->db = Database::getInstance();
-    }
+  {
+    global $usrapp;
+    $this->usrapp = $usrapp;
+    $this->db = Database::getInstance();
+  }
 
   public function authLogin($username, $userpass)
   {
-    $query = "SELECT * FROM $this->usrapp WHERE username = :username AND userpass = :userpass";
+    $query = "SELECT username, userlevel, userpass FROM $this->usrapp WHERE username = :username";
     $stmt = $this->db->prepare($query);
     $stmt->execute([
       ':username' => $username,
-      ':userpass' => $userpass,
     ]);
-    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    return $result;
+    $data = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    if ($data) {
+      $encodedPassword = base64_encode($userpass);
+      $inputHash = crypt($encodedPassword, $data['userpass']);
+
+      if ($inputHash === $data['userpass']) {
+        return $data = [
+          'username' => $data['username'],
+          'userlevel' => $data['userlevel'],
+        ];
+      }
+    }
   }
+
 
   public function authGoogleLogin($email)
   {
