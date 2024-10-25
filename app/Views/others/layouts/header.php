cfa592de3686e660fd22ffd04b5f367da7c4ab60
Samriadi <basosamriadi01@gmail.com>
2024-10-25 15:24:59 +0800

    update header

--- app/Views/others/layouts/header.php
+++ app/Views/others/layouts/header.php
@@ -24,6 +24,7 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="assets/js/helpers/function.js"></script>
 
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.1.1/crypto-js.min.js"></script>
 
 </head>
 
