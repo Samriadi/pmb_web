463154e0491d30ffab7c0b013942e21f25634efc
Samriadi <basosamriadi01@gmail.com>
2024-12-05 03:01:59 +0800

    update login model

--- app/Models/loginModel.php
+++ app/Models/loginModel.php
@@ -7,7 +7,7 @@ class loginModel
   private $mhs_dosen;
   private $mhs_staff;
   private $db;
-  
+
   public function __construct()
   {
     global $usrapp;
@@ -26,50 +26,51 @@ class loginModel
   public function authLogin($username, $userpass)
   {
       $query = "SELECT 'accounts' AS user_type, 
-                      username, 
-                      userpass, 
-                      CASE 
-                          WHEN userlevel = 'admin' THEN 'mhs' 
-                          WHEN userlevel = 'supervisor' THEN 'mhs' 
-                          ELSE 'pmb-mhs' 
-                      END AS modul, 
-                      userlevel
-                FROM $this->usrapp
-                WHERE username = :username
+                        username, 
+                        userpass, 
+                        CASE 
+                            WHEN userlevel = 'admin' THEN 'mhs' 
+                            WHEN userlevel = 'supervisor' THEN 'mhs' 
+                            ELSE 'pmb-mhs' 
+                        END AS modul, 
+                        userlevel
+                  FROM $this->usrapp
+                  WHERE username = :username
 
-                UNION
+                  UNION
 
-                SELECT 'mahasiswa' AS user_type, UserName, UserPass, 'mhs' AS modul, NULL AS userlevel
-                FROM $this->mhs_mahasiswa
-                WHERE username = :username
+                  SELECT 'mahasiswa' AS user_type, UserName, UserPass, 'mhs' AS modul, NULL AS userlevel
+                  FROM $this->mhs_mahasiswa
+                  WHERE username = :username
 
-                UNION
+                  UNION
 
-                SELECT 'staff' AS user_type, username, userpass, 'pmb' AS modul, 'staf' AS userlevel
-                FROM $this->mhs_staff
-                WHERE username = :username";
-  
-      $stmt = $this->db->prepare($query);
-      $stmt->execute([
-          ':username' => $username,
-      ]);
-      $data = $stmt->fetch(PDO::FETCH_ASSOC);
-  
-      if ($data) {
-          $encodedPassword = base64_encode($userpass); 
-          $inputHash = crypt($encodedPassword, $data['userpass']); 
-  
-          if ($inputHash === $data['userpass']) {
-              return [
-                  'username' => $data['username'],
-                  'userlevel' => $data['userlevel'],
-                  'user_type' => $data['user_type'],
-                  'modul' => $data['modul']
-              ];
-          }
+                  SELECT 'staff' AS user_type, username, userpass, 'pmb' AS modul, 'staf' AS userlevel
+                  FROM $this->mhs_staff
+                  WHERE username = :username";
+
+    $stmt = $this->db->prepare($query);
+    $stmt->execute([
+      ':username' => $username,
+    ]);
+    $data = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    if ($data) {
+      $encodedPassword = base64_encode($userpass);
+      $inputHash = crypt($encodedPassword, $data['userpass']);
+
+      if ($inputHash === $data['userpass']) {
+        return [
+          'username' => $data['username'],
+          'userlevel' => $data['userlevel'],
+          'user_type' => $data['user_type'],
+          'modul' => $data['modul']
+        ];
       }
-      return null;
+    }
+    return null;
   }
+
 
   public function authGoogleLogin($email)
   {
