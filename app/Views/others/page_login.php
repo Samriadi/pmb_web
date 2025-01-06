fc92690a0f43583042a838689ea30ff87e930f76
Samriadi <basosamriadi01@gmail.com>
2025-01-06 19:39:06 +0800

    delete header uri login google

--- app/Views/others/page_login.php
+++ app/Views/others/page_login.php
@@ -1,21 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-
 <?php
 require_once '../vendor/autoload.php';
-
-$client = new Google_Client();
-$client->setClientId('203970939775-gsku8lo8f2cf2q148m5nas8an9mlahqj.apps.googleusercontent.com');
-$client->setClientSecret('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');
-$client->setRedirectUri('http://localhost/admin/callback');
-$client->addScope("email");
-$client->addScope("profile");
-
-// Set prompt parameter to 'consent' or 'select_account'
-$client->setPrompt('select_account');
-
-$login_url = $client->createAuthUrl();
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
 
 <head>
     <meta charset="UTF-8">
@@ -34,7 +22,8 @@ $login_url = $client->createAuthUrl();
             display: flex;
             align-items: center;
             justify-content: center;
-            background: linear-gradient(135deg, #8e44ad, #3498db); /* Latar belakang gradien */
+            background: linear-gradient(135deg, #8e44ad, #3498db);
+            /* Latar belakang gradien */
             animation: fadeIn 1s ease;
         }
 
@@ -79,7 +68,8 @@ $login_url = $client->createAuthUrl();
 
         .card:hover {
             transform: translateY(-15px) scale(1.05);
-            box-shadow: 0 20px 60px rgba(0, 0, 0, 0.3); /* Increased shadow on hover */
+            box-shadow: 0 20px 60px rgba(0, 0, 0, 0.3);
+            /* Increased shadow on hover */
         }
 
         .btn-custom {
@@ -107,7 +97,7 @@ $login_url = $client->createAuthUrl();
             <div class="container">
                 <div class="row justify-content-center">
                     <div class="col-12 col-md-9 col-lg-7 col-xl-6 col-xxl-5">
-                        <div class="card border-0 shadow-lg"> <!-- Using shadow-lg for more pronounced shadow -->
+                        <div class="card border-0 shadow-lg">
                             <div class="card-body p-3 p-md-4 p-xl-5">
                                 <div class="row">
                                     <div class="col-12">
@@ -137,19 +127,6 @@ $login_url = $client->createAuthUrl();
                                         </div>
                                     </div>
                                 </form>
-                                <!-- <div class="row">
-                                    <div class="col-12">
-                                        <p class="mt-4 mb-4">Or continue with</p>
-                                        <div class="d-flex gap-3 flex-column">
-                                            <a href="<?php echo $login_url; ?>" class="btn btn-danger">
-                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-google" viewBox="0 0 16 16">
-                                                    <path d="M15.545 6.558a9.42 9.42 0 0 1 .139 1.626c0 2.434-.87 4.492-2.384 5.885h.002C11.978 15.292 10.158 16 8 16A8 8 0 1 1 8 0a7.689 7.689 0 0 1 5.352 2.082l-2.284 2.284A4.347 4.347 0 0 0 8 3.166c-2.087 0-3.86 1.408-4.492 3.304a4.792 4.792 0 0 0 0 3.063h.003c.635 1.893 2.405 3.301 4.492 3.301 1.078 0 2.004-.276 2.722-.764h-.003a3.702 3.702 0 0 0 1.599-2.431H8v-3.08h7.545z" />
-                                                </svg>
-                                                <span class="ms-2 fs-6 text-uppercase">Sign in With Google</span>
-                                            </a>
-                                        </div>
-                                    </div>
-                                </div> -->
                             </div>
                         </div>
                     </div>
@@ -161,8 +138,8 @@ $login_url = $client->createAuthUrl();
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script>
-        $(document).ready(function () {
-            $('#loginForm').submit(function (event) {
+        $(document).ready(function() {
+            $('#loginForm').submit(function(event) {
                 event.preventDefault();
                 var username = $('#username').val();
                 var userpass = $('#userpass').val();
@@ -175,7 +152,7 @@ $login_url = $client->createAuthUrl();
                         username: username,
                         userpass: userpass
                     }),
-                    success: function (response) {
+                    success: function(response) {
                         let responseObject = JSON.parse(response);
                         if (responseObject.success === true) {
                             Swal.fire({
@@ -197,7 +174,7 @@ $login_url = $client->createAuthUrl();
                             });
                         }
                     },
-                    error: function (xhr, status, error) {
+                    error: function(xhr, status, error) {
                         var errorMessage = xhr.status + ': ' + xhr.statusText;
                         $('#message').html('<p>Terjadi kesalahan saat menghubungi server. (' + errorMessage + ')</p>');
                     }
